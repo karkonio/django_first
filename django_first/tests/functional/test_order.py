b2eba3207e6641e4860655760ce82d3701f91505
KorkaYess <karina.yesbukenova@gmail.com>
2019-04-11 15:50:09 +0600

    Add tests for unconfurmed payment and multiple payments

--- django_first/tests/functional/test_order.py
+++ django_first/tests/functional/test_order.py
@@ -47,6 +47,22 @@ def test_order_process_is_ok(db, data):
     assert store_item.quantity == 90
 
 
+def test_order_process_ok_mulitple_payments(db, data):
+    product, store, store_item, order, order_item, customer, payment = data
+    payment.amount = 50
+    payment.save()
+    Payment.objects.create(
+        order=order,
+        amount=50,
+        is_confirmed=True
+    )
+    order.process()
+    store_item.refresh_from_db()
+    assert order.price == 100
+    assert order.is_paid is True
+    assert store_item.quantity == 90
+
+
 def test_order_process_fail_not_enough_stock(db, data):
     product, store, store_item, order, order_item, customer, payment = data
     order_item.quantity = 200
@@ -59,6 +75,15 @@ def test_order_process_fail_not_enough_stock(db, data):
 def test_order_process_fail_not_enough_money(db, data):
     product, store, store_item, order, order_item, customer, payment = data
     payment.amount = 10
+    payment.save()
+    with pytest.raises(Exception) as e:
+        order.process()
+    assert str(e.value) == 'Not enough money'
+
+
+def test_order_process_fail_payment_not_confirmed(db, data):
+    product, store, store_item, order, order_item, customer, payment = data
+    payment.is_confirmed = False
     payment.save()
     with pytest.raises(Exception) as e:
         order.process()
