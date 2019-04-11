5a4df3ef3fea981127b6b8fd52bdab75516f8f50
KorkaYess <karina.yesbukenova@gmail.com>
2019-04-11 18:54:23 +0600

    Add admin an delete wrong nulls

--- django_first/models.py
+++ django_first/models.py
@@ -4,8 +4,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(
-        max_digits=10, decimal_places=2,
-        blank=True, null=True
+        max_digits=10, decimal_places=2
     )
 
 
@@ -32,8 +31,7 @@ class StoreItem(models.Model):
 class Order(models.Model):
     customer = models.ForeignKey(
         Customer, on_delete=models.CASCADE,
-        related_name='orders',
-        null=True, blank=True
+        related_name='orders'
     )
     location = models.CharField(max_length=100)
     price = models.DecimalField(
