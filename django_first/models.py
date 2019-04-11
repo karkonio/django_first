29d4157093c8ae169146b77f9a534b0168562d61
KorkaYess <karina.yesbukenova@gmail.com>
2019-04-11 13:41:35 +0600

    Add Store object

--- django_first/models.py
+++ django_first/models.py
@@ -2,4 +2,13 @@ from django.db import models
 
 
 class Product(models.Model):
-	name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+
+
+class Store(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE,
+        related_name='stores'
+    )
+    quantity = models.IntegerField()
+    location = models.CharField(max_length=100)
