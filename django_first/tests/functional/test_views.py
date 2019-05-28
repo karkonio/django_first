b396d2ac848c3c7dad2d81bc56ebb064358c1620
KorkaYess <karina.yesbukenova@gmail.com>
2019-05-28 17:28:23 +0600

    Add list for orders

--- django_first/tests/functional/test_views.py
+++ django_first/tests/functional/test_views.py
@@ -1,3 +1,8 @@
+from lxml import html
+
+from django_first.models import Order
+
+
 def test_hello(db, client, data):
     client.login(username='kira', password='testtest')
     response = client.get('/')
@@ -5,3 +10,6 @@ def test_hello(db, client, data):
     response = response.content.decode('utf-8')
     assert 'Hello, World!' in response
     assert 'kira' in response
+    response = html.fromstring(response)
+    orders = Order.objects.filter(customer__user__username='kira')
+    assert len(response.cssselect('li')) == orders.count()
