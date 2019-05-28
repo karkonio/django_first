569954f05dc26c87f63544032abd12b018dc9315
KorkaYess <karina.yesbukenova@gmail.com>
2019-05-28 11:50:32 +0600

    Add "templates" folder

--- django_first/tests/functional/test_django.py
+++ django_first/tests/functional/test_django.py
@@ -1,4 +1,5 @@
 def test_hello(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.content == b'Hello, World!'
+    response = response.content.decode('utf-8')
+    assert 'Hello, World!' in response
