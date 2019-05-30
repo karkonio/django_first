67de7b0aa0acf462ed2209d6372b2cd6a477f331
KorkaYess <karina.yesbukenova@gmail.com>
2019-05-30 12:57:55 +0600

    Add test for POST request to add an item to an order

--- django_first/tests/functional/test_views.py
+++ django_first/tests/functional/test_views.py
@@ -1,7 +1,7 @@
 from lxml import html
 
 from django.urls import reverse
-from django_first.models import Order
+from django_first.models import Order, Product
 
 
 def test_hello(db, client, data):
@@ -23,4 +23,18 @@ def test_order(db, client, data):
     response = response.content.decode('utf-8')
     response = html.fromstring(response)
     items = response.cssselect('.list-group-item')
+    assert len(items) == 1
     assert items[0].text == 'apple 10'
+
+
+def test_order_add(db, client, data):
+    url = reverse('order', args=[1])
+    banana = Product.objects.create(name='banana', price=15)
+    response = client.get.post(url, {'product_id': banana.id, 'quantity': 20})
+    assert response.status_code == 200
+    response = response.content.decode('utf-8')
+    response = html.fromstring(response)
+    items = response.cssselect('.list-group-item')
+    assert len(items) == 2
+    assert items[0].text == 'apple 10'
+    assert items[1].text == 'banana 20'
