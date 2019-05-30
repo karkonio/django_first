8d6b701d47a1312d12b1c68903667f23f3512e84
KorkaYess <karina.yesbukenova@gmail.com>
2019-05-30 14:19:18 +0600

    Add try-except logic to product_id in POST requests + tests

--- django_first/views.py
+++ django_first/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import Order, OrderItem
+from .models import Order, OrderItem, Product
 
 
 def hello(request):
@@ -17,6 +17,20 @@ def order(request, order_id):
             product_id = request.POST.get('product')
             quantity = request.POST.get('quantity')
             try:
+                product_id = int(product_id)
+            except ValueError:
+                return HttpResponse(
+                    'Invalid product id',
+                    status=400
+                )
+            try:
+                product = Product.objects.get(id=product_id)
+            except Product.DoesNotExist:
+                return HttpResponse(
+                    "Product doesn't exist",
+                    status=404
+                )
+            try:
                 quantity = int(quantity)
             except ValueError:
                 return HttpResponse(
@@ -30,7 +44,7 @@ def order(request, order_id):
                 )
             OrderItem.objects.create(
                 order=order,
-                product_id=product_id,
+                product=product,
                 quantity=int(quantity)
             )
     return render(request, 'order.html', context={
